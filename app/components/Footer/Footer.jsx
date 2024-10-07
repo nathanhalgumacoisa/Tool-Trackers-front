d3313ece1f8bd3dd1ad89306367859d59a06ff0a
lmcomito <lmota6789senai@gmail.com>
2024-10-07 15:30:02 -0300

    feat: footer testing

---
+++ app/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './footer.module.css';
+
+const Footer = () => {
+  return (
+    <div className={style.container}>
+      Todos os direitos reservados
+    </div>
+  );
+}
+ export default Footer
