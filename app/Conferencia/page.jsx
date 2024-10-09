838bbfb2ee65b31943afd9b8800fcb25372a8a1e
jumartinszz <juliamartinss.564@gmil.com>
2024-10-09 09:01:35 -0300

    redirecionamento pagina conferencia

--- app/Conferencia/page.jsx
+++ app/Conferencia/page.jsx
@@ -1,28 +1,13 @@
 "use client"
-import styles from './conferencia.module.css';
 import React from 'react';
-import Card from '../components/componente-cardfoto/cardfoto.jsx'
 
-
-const conferencia = () => {
+const Conferencia = () => {
     return (
-      <div style={styles.container}>
-        
-        <Card></Card>
-        <Card></Card>
-        <div style={styles.content}>
-          <h1>Conferências</h1>
-          <p>Em qual setor você gostaria de fazer a conferência?</p>
-          <div style={styles.buttonContainer}>
-            <button style={styles.button}>Mecânica de Usinagem</button>
-            <button style={styles.button}>Elétrica</button>
-            <button style={styles.button}>Espaço Maker</button>
-            <button style={styles.button}>Sala de Manutenção</button>
-          </div>
+        <div>
+            <h1>Página de Conferencia</h1>
+            {/* Conteúdo da página de empréstimos */}
         </div>
-      </div>
     );
-  };
+};
 
-
-  export default conferencia;
+export default Conferencia;
