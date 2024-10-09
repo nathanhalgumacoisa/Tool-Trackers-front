3a983ae54caee266771dbfca32eec649376f7f95
anaxcavalcante <ana.reiss@outook.com.br>
2024-10-09 15:22:31 -0300

    quase finalizado ferramenta

--- app/components/ferramentas-cards/ferramenta.jsx
+++ app/components/ferramentas-cards/ferramenta.jsx
@@ -12,8 +12,11 @@ const App = () => (
     }}
     cover={<img alt="foto da ferramenta" src="" />}
   >
+   
     <Meta title="Nome da ferramenta" description="Disponível..." />
-    
+     <img src="./chave-inglesa.png" alt="ícone" style={{ width: '15%', height: '15%', marginTop: '10%' }} />
+     <img src="./contorno-do-circulo.png" alt="ícone" style={{ width: '13%', height: '13%', marginTop: '5%', marginLeft: '10%' }} />
+
   </Card>
 );
 export default App;
