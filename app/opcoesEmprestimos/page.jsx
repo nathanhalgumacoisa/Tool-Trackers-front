4115fb342bf048171b759aa05da652cdc33a3455
lia509 <giuliarodrigues@aluno.senai.br>
2024-10-16 07:50:58 -0300

    renderizaçao de cada organizador em uma mesma pagina

--- app/opcoesEmprestimos/page.jsx
+++ app/opcoesEmprestimos/page.jsx
@@ -7,7 +7,7 @@ const OpcoesEmprestimos = () => {
         <div>
             <h1 className={styles.cards1}>O que deseja fazer ?</h1>
             <div className={styles.cardsContainer}> {/* Contêiner flexível para os cards */}
-                <App title="Oficina Mecânica de Usinagem" link="/Organizadores?ambiente=Oficina" /> {/* Primeiro card */}
+                <App title="Oficina Mecânica de Usinagem" link="/Organizadores?ambiente=ofm" /> {/* Primeiro card */}
                 <App title="Oficina Eletro Eletrônica" link="/Organizadores?ambiente=oee" /> {/* Segundo card */}
                 <App title="Espaço Maker" link="/Organizadores?ambiente=em" /> {/* Primeiro card */}
                 <App title="Manutenção" link="/Organizadores?ambiente=manut" /> {/* Primeiro card */}
