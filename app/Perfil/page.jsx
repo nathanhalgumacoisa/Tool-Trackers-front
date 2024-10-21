c51990040f7b275b14aad5ce735d4d7bb12773ef
stefanny <stefannyoliveira@aluno.senai.br>
2024-10-21 09:29:44 -0300

    front

--- app/Perfil/page.jsx
+++ app/Perfil/page.jsx
@@ -1,60 +1,53 @@
 "use client";
 
 import React, { useEffect, useState } from 'react';
-import { Card, Avatar, Typography, Spin } from 'antd';
-import styles from './perfil.module.css'
-import Header from '../components/header/Header';
+import { Card, Typography } from 'antd';
 
+import styles from './perfil.module.css';
+import Header from '../components/header/Header';
 
 const { Title, Paragraph } = Typography;
 
 const Perfil = () => {
   const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchUserData = async () => {
       try {
-        // Simulando uma chamada a uma API
         const response = await fetch('https://jsonplaceholder.typicode.com/users/1');
         const data = await response.json();
-        
-        // Simulando que a imagem do avatar é obtida de outro lugar ou é fixa
+
         const userData = {
           name: data.name,
           email: data.email,
-          ambienteResponsavel: data.ambiente, //puxar o ambiente
-          avatar: 'https://via.placeholder.com/150',
+          ambienteResponsavel: 'Ambiente de Desenvolvimento', // vai pegar do back
         };
 
         setUser(userData);
       } catch (error) {
         console.error('Erro ao buscar os dados do usuário:', error);
-      } finally {
-        setLoading(false);
       }
     };
 
     fetchUserData();
   }, []);
 
-  if (loading) {
-    return <Spin size="large" />;
-  }
-
   return (
-    <div >
-        <Header />
+    <div>
+      <Header />
 
-        <div className={styles.classe}>
-      <Card style={{ width: 300, textAlign: 'center' }}>
-        <Avatar size={100} src={user.avatar} />
-        <Title level={4}>{user.name}</Title>
-        <Paragraph>{user.email}</Paragraph>
-        <Paragraph>{user.ambiente}</Paragraph>
-      </Card>
+      <div className={styles.classe}>
+        {user && (
+          <Card style={{ width: 300, textAlign: 'center' }}>
+            {/* <div style={{ width: 100, height: 100, margin: '0 auto' }}>
+              <User size={100} color="#555" /> 
+            </div> user icone*/}
+            <Title level={4}>{user.name}</Title>
+            <Paragraph>{user.email}</Paragraph>
+            <Paragraph>{user.ambienteResponsavel}</Paragraph>
+          </Card>
+        )}
       </div>
-
     </div>
   );
 };
