903b8f8e56cfa6ac8fae241ae35c626f8662e2bf
thaynVazzoler <thayna.vazzoler@aluno.senai.br>
2024-10-21 08:21:15 -0300

    render organizador

--- app/Desejafazer/page.jsx
+++ app/Desejafazer/page.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import App from '../components/card/Card'; // Ajuste o caminho conforme necessário
 import styles from './deseja.module.css'; // Importando o CSS Module
+import Header from '../components/header/Header.jsx';
+
 
 const Desejafazer = () => {
     return (
         <div>
+            <Header/>
             <h1 className={styles.cards1}>O que deseja fazer ?</h1>
             <div className={styles.cardsContainer}> {/* Contêiner flexível para os cards */}
                 <App title="Empréstimo" link="/opcoesEmprestimos" /> {/* Primeiro card */}
-                <App title="Conferência" link="/opcoesConferencia" /> {/* Segundo card */}
+                <App title="Conferência" link="/OpcoesConferencias" /> {/* Segundo card */}
             </div>
         </div>
     );
