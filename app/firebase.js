75c24fa75d29522d31afb60781f3ecb4e782fc09
Stefanny Rodrigues <stefannyoliveira@aluno.senai.br>
2024-10-28 14:44:07 -0300

    .env

--- app/firebase.js
+++ app/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey:process.env.NEXT_PUBLIC_FIREBASE_APIKEY, 
-  authDomain:process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
-  projectId:process.env.NEXT_PUBLIC_FIREBASE_PROJECTID ,
-  storageBucket:process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
-  messagingSenderId:process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
-  appId:process.env.NEXT_PUBLIC_FIREBASE_APPID,
+  apiKey:process.env.NEXT_PUBLIC_FIREBASE_API_KEY, 
+  authDomain:process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId:process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket:process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDER_ID,
+  appId:process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 
