24ccc4aa78d7f632bf3721970f615306bf29b014
lia509 <giuliarodrigues@aluno.senai.br>
2024-10-30 17:04:20 -0300

    cadeia de dropdown localização => ferramentas

--- app/Organizadores/page.jsx
+++ app/Organizadores/page.jsx
@@ -3,9 +3,8 @@ import { useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-import Dropdown from '../components/dropdown/Dropdown.jsx';
 import Header from '../components/header/Header.jsx';
-import CompLoc from '../components/comploc/CompLoc.jsx';
+import OrganizadorComponent from '../components/dropOrganizadores/DropOrganizadores.jsx';
 
 function App() {
   const [locals, setLocals] = useState([]);
@@ -28,39 +27,20 @@ function App() {
     getOrganizador();
   }, [ambiente]);
 
+  // Filtrando organizadores por tipo
+  const organizadores = {
+    carrinhos: locals.filter(l => l.nome_organizador === 'carrinhos'),
+    armarios: locals.filter(l => l.nome_organizador === 'armarios'),
+    tornos: locals.filter(l => l.nome_organizador === 'tornos'),
+    paineis: locals.filter(l => l.nome_organizador === 'paineis'),
+  };
+
   return (
     <div className="App">
       <Header />
-      {locals.map((l) => (
-        <CompLoc
-        key={l.localizacoes_id}
-        ambiente={l.ambiente}
-        nome_organizador={l.nome_organizador}
-        numero_organizador={l.numero_organizador}
-        nome_suborganizador={l.nome_suborganizador}
-        numero_suborganizador={l.numero_suborganizador}
-        foto_url={l.foto_url}
-    />
-  // <div key={l.localizacao_id}>
-  //   <h3>{l.ambiente}</h3>
-    
-  //   {/* Renderizando organizador */}
-  //   <div>
-  //     <h4>Organizador:</h4>
-  //     <p>{l.nome_organizador} {l.numero_organizador}</p>
-  //   </div>
-  //   <div>
-  //     <h4>Sub-Organizador:</h4>
-  //     <p>{l.nome_suborganizador} {l.numero_organizador} {l.foto_url && (
-  //                 <img src={l.foto_url} alt={l.nome_suborganizador} style={{ width: '50px', height: '50px', marginLeft: '10px' }} />
-  //               )}</p>
-  //   </div>
-
-    
-  // </div>
-))}
-
-      {/* <Dropdown title={['Carrinhos']} /> */}
+      
+      {/* Componente de Organizadores com dados filtrados */}
+      <OrganizadorComponent organizadores={organizadores} />
     </div>
   );
 }
