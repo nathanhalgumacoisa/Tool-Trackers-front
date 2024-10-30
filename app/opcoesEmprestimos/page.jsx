db68ad72a54dcfc30a8baefb23507b8850f11380
Stefanny44 <123407270+Stefanny44@users.noreply.github.com>
2024-10-30 11:31:20 -0300

    opcoes emprestimos

--- app/opcoesEmprestimos/page.jsx
+++ app/opcoesEmprestimos/page.jsx
@@ -1,17 +1,28 @@
 import App from '../components/card/Card'; // Ajuste o caminho conforme necessário
 import styles from './opcoesemprest.module.css'; // Importando o CSS Module
+import Header from '../components/header/Header.jsx';
 
 const OpcoesEmprestimos = () => {
     
     return (
         <div>
+            <div className={styles.header}>
+                <Header/>
+            </div>
+            <div className={styles.div1}>
+            <img src="./fundosenai.png" alt="Fundo Senai" className={styles.imagem1} />
+            </div>
+
             <h1 className={styles.cards1}>O que deseja fazer ?</h1>
             <div className={styles.cardsContainer}> {/* Contêiner flexível para os cards */}
+                <div>
                 <App title="Oficina Mecânica de Usinagem" link="/Organizadores?ambiente=ofm" /> {/* Primeiro card */}
                 <App title="Oficina Eletro Eletrônica" link="/Organizadores?ambiente=oee" /> {/* Segundo card */}
+                </div>
+                <div>
                 <App title="Espaço Maker" link="/Organizadores?ambiente=em" /> {/* Primeiro card */}
                 <App title="Manutenção" link="/Organizadores?ambiente=manut" /> {/* Primeiro card */}
-
+                </div>
             </div>
         </div>
     );
