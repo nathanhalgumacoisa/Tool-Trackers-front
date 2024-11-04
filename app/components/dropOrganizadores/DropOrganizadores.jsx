cc59b323261953bb62f45add705782bfb6b6bde1
lia509 <giuliarodrigues@aluno.senai.br>
2024-11-04 15:39:34 -0300

    drop

--- app/components/dropOrganizadores/DropOrganizadores.jsx
+++ app/components/dropOrganizadores/DropOrganizadores.jsx
@@ -24,26 +24,32 @@ const OrganizadorComponent = ({ organizadores }) => {
             {organizadores.carrinhos.map(org => (
               <Option key={org.numero_organizador} value={org.numero_organizador}>
                 {org.numero_organizador}
+              </Option>,
+
+              <Option key={org.numero_suborganizador} value={org.numero_suborganizador}>
+                {org.numero_suborganizador}
               </Option>
+
+
             ))}
           </Select>
-          {selectedCarrinhos && (
+          {/* {selectedCarrinhos && (
             <Collapse style={{ marginTop: '10px' }}>
               {organizadores.carrinhos
                 .filter(org => org.numero_organizador === selectedCarrinhos)
                 .flatMap(org => 
                   Array.isArray(org.sub_organizadores) && org.sub_organizadores.length > 0
                     ? org.sub_organizadores.map(sub => (
-                        <Panel header={`Gaveta ${sub.numero}`} key={sub.numero}>
+                        <Panel header={`Gaveta ${sub.numero_suborganizador}`} key={sub.numero_suborganizador}>
                           <p>{sub.detalhes || 'Sem detalhes disponíveis.'}</p>
                         </Panel>
                       ))
-                    : <Panel header="Nenhuma gaveta disponível" key="no-gavetas">
+                    : [<Panel header="Nenhuma gaveta disponível" key="no-gavetas">
                         <p>Não há gavetas para este carrinho.</p>
-                      </Panel>
+                      </Panel>]
                 )}
             </Collapse>
-          )}
+          )} */}
         </Col>
 
         {/* Coluna Armários */}
@@ -68,13 +74,13 @@ const OrganizadorComponent = ({ organizadores }) => {
                 .flatMap(org => 
                   Array.isArray(org.sub_organizadores) && org.sub_organizadores.length > 0
                     ? org.sub_organizadores.map(sub => (
-                        <Panel header={`Prateleira ${sub.numero}`} key={sub.numero}>
+                        <Panel header={`Prateleira ${sub.numero_suborganizador}`} key={sub.numero_suborganizador}>
                           <p>{sub.detalhes || 'Sem detalhes disponíveis.'}</p>
                         </Panel>
                       ))
-                    : <Panel header="Nenhuma prateleira disponível" key="no-prateleiras">
+                    : [<Panel header="Nenhuma prateleira disponível" key="no-prateleiras">
                         <p>Não há prateleiras para este armário.</p>
-                      </Panel>
+                      </Panel>]
                 )}
             </Collapse>
           )}
