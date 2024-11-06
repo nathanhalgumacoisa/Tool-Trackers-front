a2b79f8750ab3b49dc079f5a2b39d06d18050f94
Livia Mota <lmota6780senai@gmail.com>
2024-11-06 11:40:04 -0300

    feat: linkando as páginas no menu

--- app/components/menu/Menu.jsx
+++ app/components/menu/Menu.jsx
@@ -6,16 +6,16 @@ const Menu = () => {
     return (
         <div className={styles.menu}>
             <div className={styles.container}>
-              <p>Cadastro de Ferramentas</p>
+              <Link href="/CadastroFerr"><p>Cadastro de Ferramentas</p></Link>
             </div>
             <div className={styles.container}>
-              <p>Cadastro Usuário</p>
+              <Link href="/CadastroUsuario"><p>Cadastro Usuário</p></Link>
+            </div> 
+            <div className={styles.container}>
+              <Link href="/Conferencia"><p>Conferência</p></Link>
             </div>
             <div className={styles.container}>
-              <p>Conferência</p>
-            </div>
-            <div className={styles.container}>
-              <p>Empréstimo</p>
+                <Link href="/Emprestimos"><p>Empréstimo</p></Link>
             </div>   
         </div>
     );
