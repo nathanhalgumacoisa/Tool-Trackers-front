7d4cb461a2677a648719e291308710f234fc5c22
lia509 <giuliarodrigues@aluno.senai.br>
2024-11-18 17:18:26 -0300

    renderizando as ferramentas pelo organizador

--- app/Organizadores/page.jsx
+++ app/Organizadores/page.jsx
@@ -4,15 +4,13 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import styles from './organizadores.module.css';
 import Header from '../components/header/Header.jsx';
+import CardFerr from '../components/cardFerramenta/CardFerr';
 
 function Organizadores() {
   const [locals, setLocals] = useState([]);
   const [expanded, setExpanded] = useState({});
   const [expandedCarrinhos, setExpandedCarrinhos] = useState(false);
   const [expandedArmarios, setExpandedArmarios] = useState(false);
-  const [expandedTornos, setExpandedTornos] = useState(false);
-  const [expandedPaineis, setExpandedPaineis] = useState(false);
-  const [selectedCarrinho, setSelectedCarrinho] = useState(""); // Estado para o carrinho selecionado
   const searchParams = useSearchParams();
   const ambiente = searchParams.get('ambiente');
 
@@ -33,76 +31,73 @@ function Organizadores() {
   const handleNumOrganizadorClick = (id) => {
     setExpanded((prev) => ({
       ...prev,
-      [id]: { ...prev[id], sub: !prev[id]?.sub } // alterna a visibilidade do suborganizador
+      [id]: { ...prev[id], sub: !prev[id]?.sub }
     }));
   };
 
-  const handleSelectChange = (event) => {
-    const selectedId = event.target.value;
-    setSelectedCarrinho(selectedId);
-    handleNumOrganizadorClick(selectedId); // Expande ou colapsa o carrinho selecionado
-  };
   const handleImgOrganizadorClick = (id) => {
     setExpanded((prev) => ({
       ...prev,
       [id]: { ...prev[id], img: !prev[id]?.img }
     }));
-    if (!expanded[id]?.img) {
-      getFerramentas(id);
-    } else {
-      setFerramentas([]);
-    }
   };
+
+  const handleFerramentasClick = (id) => {
+    setExpanded((prev) => ({
+      ...prev,
+      [id]: { ...prev[id], ferr: !prev[id]?.ferr }
+    }));
+    console.log("cricou");
+  };
+
 
   return (
     <div className={styles.container} key="app-container">
       <Header />
       <div className={styles.localsContainer}>
-        
-      <div className={styles.selectedCarrinho}>
-  <div className={styles.h2Container}>
-    <select 
-      className={styles.h2} 
-      onClick={() => setExpandedCarrinhos(!expandedCarrinhos)} 
-      onChange={handleSelectChange}
-      value={selectedCarrinho}
-    >
-      <option value="">Carrinhos</option>
-      {expandedCarrinhos && locals
-        .filter(local => local.nome_organizador === 'carrinhos')
-        .map((local, index) => (
-          <option 
-            key={`carrinho-${index}-${local.organizador_id}`} 
-            value={local.id}
-          >
-            {local.numero_organizador}
-          </option>
-        ))}
-    </select>
 
-    {/* Exibição do Carrinho Selecionado */}
-    {selectedCarrinho && (
-      <div className={styles.selectedCarrinho}>
-        <p onClick={() => handleNumOrganizadorClick(selectedCarrinho)}>
-          Carrinho selecionado: {selectedCarrinho}
-        </p>
-      </div>
-    )}
+        {/* Seção Carrinhos */}
+        <div className={styles.h2Container}>
+          <h2 className={styles.h2} onClick={() => setExpandedCarrinhos(!expandedCarrinhos)}>Carrinhos</h2>
+          {expandedCarrinhos && locals
+            .filter(local => local.nome_organizador === 'carrinhos')
+            .map((local, index) => (
+              <div key={`carrinho-${index}-${local.organizador_id}`} className={styles.mapItem}>
+                <p onClick={() => handleNumOrganizadorClick(local.id)} className={styles.mapText}>
+                  {local.numero_organizador}
+                </p>
+                {expanded[local.id]?.sub && (
+                  <p onClick={() => handleImgOrganizadorClick(local.id)} className={styles.mapText}>
+                    {local.nome_suborganizador} {local.numero_suborganizador}
+                  </p>
+                )}
+                {expanded[local.id]?.img && (
+                  <img
+                    onClick={() => handleFerramentasClick(local.id)}
+                    style={{ cursor: 'pointer' }}
+                    src={local.foto_url}
+                    className={styles.mapImage}
+                    alt="Imagem do organizador"
 
-    {/* Exibição do Suborganizador se `expanded` estiver ativo */}
-    {expanded[selectedCarrinho]?.sub && locals
-      .filter(local => local.id === selectedCarrinho)
-      .map((local) => (
-        <p 
-          key={`suborganizador-${local.id}`}
-          onClick={() => handleImgOrganizadorClick(local.id)} 
-          className={styles.mapText}
-        >
-          {local.nome_suborganizador} {local.numero_suborganizador}
-        </p>
-      ))}
-  </div>
-</div>
+                  />
+                )}
+                {expanded[local.id]?.ferr && (
+                 <CardFerr
+                 
+                 nome={local.nome}
+                 imagem_url={local.imagem_url}
+                 conjunto={local.conjunto}
+                 numero={local.numero}
+                 patrimonio={local.patrimonio}
+                 modelo={local.modelo}
+                 descricao={local.descricao}
+               />
+                )}
+              </div>
+            ))} 
+
+        </div>
+
         {/* Seção Armários */}
         <div className={styles.h2Container}>
           <h2 className={styles.h2} onClick={() => setExpandedArmarios(!expandedArmarios)}>Armários</h2>
@@ -119,7 +114,26 @@ function Organizadores() {
                   </p>
                 )}
                 {expanded[local.id]?.img && (
-                  <img src={local.foto_url} className={styles.mapImage} alt="Imagem do organizador" />
+                  <img
+                    onClick={() => handleFerramentasClick(local.id)}
+                    style={{ cursor: 'pointer' }}
+                    src={local.foto_url}
+                    className={styles.mapImage}
+                    alt="Imagem do organizador"
+
+                  />
+                )}
+                {expanded[local.id]?.ferr && (
+                 <CardFerr
+                 
+                 nome={local.nome}
+                 imagem_url={local.imagem_url}
+                 conjunto={local.conjunto}
+                 numero={local.numero}
+                 patrimonio={local.patrimonio}
+                 modelo={local.modelo}
+                 descricao={local.descricao}
+               />
                 )}
               </div>
             ))}
@@ -127,8 +141,8 @@ function Organizadores() {
 
         {/* Seção Tornos */}
         <div className={styles.h2Container}>
-          <h2 className={styles.h2} onClick={() => setExpandedTornos(!expandedTornos)}>Tornos</h2>
-          {expandedTornos && locals
+          <h2 className={styles.h2} onClick={() => setExpanded((prev) => ({ ...prev, tornos: !prev.tornos }))}>Tornos</h2>
+          {expanded.tornos && locals
             .filter(local => local.nome_organizador === 'tornos')
             .map((local, index) => (
               <div key={`torno-${index}-${local.organizador_id}`} className={styles.mapItem}>
@@ -140,8 +154,27 @@ function Organizadores() {
                     {local.nome_suborganizador} {local.numero_suborganizador}
                   </p>
                 )}
-                {expanded[local.id]?.img && (
-                  <img src={local.foto_url} className={styles.mapImage} alt="Imagem do organizador" />
+               {expanded[local.id]?.img && (
+                  <img
+                    onClick={() => handleFerramentasClick(local.id)}
+                    style={{ cursor: 'pointer' }}
+                    src={local.foto_url}
+                    className={styles.mapImage}
+                    alt="Imagem do organizador"
+
+                  />
+                )}
+                {expanded[local.id]?.ferr && (
+                 <CardFerr
+                 
+                 nome={local.nome}
+                 imagem_url={local.imagem_url}
+                 conjunto={local.conjunto}
+                 numero={local.numero}
+                 patrimonio={local.patrimonio}
+                 modelo={local.modelo}
+                 descricao={local.descricao}
+               />
                 )}
               </div>
             ))}
@@ -149,8 +182,8 @@ function Organizadores() {
 
         {/* Seção Paineis */}
         <div className={styles.h2Container}>
-          <h2 className={styles.h2} onClick={() => setExpandedPaineis(!expandedPaineis)}>Paineis</h2>
-          {expandedPaineis && locals
+          <h2 className={styles.h2} onClick={() => setExpanded((prev) => ({ ...prev, paineis: !prev.paineis }))}>Paineis</h2>
+          {expanded.paineis && locals
             .filter(local => local.nome_organizador === 'paineis')
             .map((local, index) => (
               <div key={`painel-${index}-${local.organizador_id}`} className={styles.mapItem}>
@@ -163,7 +196,26 @@ function Organizadores() {
                   </p>
                 )}
                 {expanded[local.id]?.img && (
-                  <img src={local.foto_url} className={styles.mapImage} alt="Imagem do organizador" />
+                  <img
+                    onClick={() => handleFerramentasClick(local.id)}
+                    style={{ cursor: 'pointer' }}
+                    src={local.foto_url}
+                    className={styles.mapImage}
+                    alt="Imagem do organizador"
+
+                  />
+                )}
+                {expanded[local.id]?.ferr && (
+                 <CardFerr
+                 
+                 nome={local.nome}
+                 imagem_url={local.imagem_url}
+                 conjunto={local.conjunto}
+                 numero={local.numero}
+                 patrimonio={local.patrimonio}
+                 modelo={local.modelo}
+                 descricao={local.descricao}
+               />
                 )}
               </div>
             ))}
