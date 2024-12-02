6a4b4df970ff1316ea2fd6328789cef45c688bc9
nathanhalgumacoisa <nathan.henrique@aluno.senai.br>
2024-12-02 09:36:10 -0300

    descomponentizando o menu pq estava dando erro

--- app/components/header/Header.jsx
+++ app/components/header/Header.jsx
@@ -1,12 +1,26 @@
+import React from 'react'
 import styles from './header.module.css'
-import Menu from '../menu/Menu'
+import Link from 'next/link'
 
 const Header = () => {
     
     return(
         <div className={styles.container}> 
             <img src="https://desenvolveitapevi.wordpress.com/wp-content/uploads/2016/02/logo-senai1.png" alt="logo senai" className={styles.img} />
-            <Menu />
+            <div className={styles.menu}>
+                <div className={styles.text_container}>
+                    <Link href="/CadastroFerr"><p>Cadastro de Ferramentas</p></Link>
+                </div>
+                 <div className={styles.text_container}>
+                    <Link href="/Cadastro"><p>Cadastro Usuário</p></Link>
+                </div> 
+                <div className={styles.text_container}>
+                    <Link href="/Desejafazer"><p>Deseja Fazer</p></Link>
+                </div>
+                <div className={styles.text_container}>
+                    <Link href="/Ferramentas"><p>Ferramentas</p></Link>
+                </div>   
+            </div>
         </div>
     )
 } 
