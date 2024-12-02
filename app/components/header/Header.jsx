93f31745feabded0fe17ad46fa5247b91e4fc325
Nathan Oliveira <nathan.henrique@aluno.senai.br>
2024-12-02 16:50:25 -0300

    vou subirrr

--- app/components/header/Header.jsx
+++ app/components/header/Header.jsx
@@ -3,25 +3,25 @@ import styles from './header.module.css'
 import Link from 'next/link'
 
 const Header = () => {
-    
-    return(
+    return (
         <div className={styles.container}> 
-            <img src="https://desenvolveitapevi.wordpress.com/wp-content/uploads/2016/02/logo-senai1.png" alt="logo senai" className={styles.img} />
-            <div className={styles.menu}>
-                <div className={styles.text_container_ferr}>
-                    <Link href="/CadastroFerr"><p>Cadastro de Ferramentas</p></Link>
-                </div>
-                 <div className={styles.text_container}>
-                    <Link href="/Usuarios"><p>Gerenciamento de Usuário</p></Link>
-                </div> 
-                <div className={styles.text_container}>
-                    <Link href="/Inventario"><p>inventário</p></Link>
-                </div>
-                <div className={styles.text_container}>
-                    <Link href="/Ferramentas"><p>Ferramentas</p></Link>
-                </div>   
+            <Link href="/">
+                    <img src="https://desenvolveitapevi.wordpress.com/wp-content/uploads/2016/02/logo-senai1.png" alt="logo senai" className={styles.img} />
+            </Link>
+            <div className={styles.text_container_ferr}>
+                <Link href="/CadastroFerr"><p>Cadastro de Ferramentas</p></Link>
             </div>
+            <div className={styles.text_container}>
+                <Link href="/GerenciamentoUsuarios"><p>Gerenciamento de Usuário</p></Link>
+            </div> 
+            <div className={styles.text_container}>
+                <Link href="/inventario"><p>Inventário</p></Link>
+            </div>
+            <div className={styles.text_container}>
+                <Link href="/Ferramentas"><p>Ferramentas</p></Link>
+            </div>   
         </div>
     )
-} 
+}
+
 export default Header
