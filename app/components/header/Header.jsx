82f429ad9142dd9911437a6cfa8f1d6f16aec387
Nathan Oliveira <nathan.henrique@aluno.senai.br>
2024-12-02 16:25:05 -0300

    .

--- app/components/header/Header.jsx
+++ app/components/header/Header.jsx
@@ -12,10 +12,10 @@ const Header = () => {
                     <Link href="/CadastroFerr"><p>Cadastro de Ferramentas</p></Link>
                 </div>
                  <div className={styles.text_container}>
-                    <Link href="/Cadastro"><p>Cadastro Usuário</p></Link>
+                    <Link href="/Usuarios"><p>Gerenciamento de Usuário</p></Link>
                 </div> 
                 <div className={styles.text_container}>
-                    <Link href="/Desejafazer"><p>Deseja Fazer</p></Link>
+                    <Link href="/Inventario"><p>inventário</p></Link>
                 </div>
                 <div className={styles.text_container}>
                     <Link href="/Ferramentas"><p>Ferramentas</p></Link>
